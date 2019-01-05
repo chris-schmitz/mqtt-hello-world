4996b941a5cd2944487e0a7860577d2f7c9c49f2
chris-schmitz <schmitz.chris@gmail.com>
2019-01-05 14:30:58 -0600

    Adds a color picker tab that will set the color of the LED

--- nativescript-client/app/components/Picker.vue
+++ nativescript-client/app/components/Picker.vue
@@ -3,14 +3,19 @@
     <ActionBar title="Color Picker"/>
     <Button text="Show RGB Picker" @tap="showColorPicker"/>
     <StackLayout orientation="horizontal">
-      <Label text="Selected Color: " class="message" textWrap="true"/>
-      <Label :text="selectedColor" class="message" textWrap="true"/>
+      <Label class="selected-color">
+        <FormattedString>
+          <Span class="label" text="The selected color is: "/>
+          <Span class="value" :style="valueColor" :text="selectedColor"/>
+        </FormattedString>
+      </Label>
     </StackLayout>
   </StackLayout>
 </template>
 
 <script>
 import { ColorPicker } from "nativescript-color-picker";
+import { Color } from "tns-core-modules/Color";
 
 export default {
   data() {
@@ -19,14 +24,19 @@ export default {
       selectedColor: null
     };
   },
+  computed: {
+    valueColor() {
+      return {
+        color: this.selectedColor ? this.selectedColor.hex : ""
+      };
+    }
+  },
   methods: {
     showColorPicker() {
-      console.log(this.picker.show);
       this.picker
         .show("#3489db", "RGB")
         .then(result => {
-          console.log("color int: " + result);
-          this.selectedColor = result;
+          this.selectedColor = new Color(result);
         })
         .catch(err => {
           console.log(err);
@@ -40,6 +50,10 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-.picker-container {
+.selected-color {
+  .value {
+    font-weight: bold;
+    font-size: 50px;
+  }
 }
 </style>
